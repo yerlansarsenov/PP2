fb4e97979e50d6f6b3f89b533d1d966fcad54378
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-01-26 15:28:38 +0600

    Add files via upload

---
+++ Week 1/Task2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        class Student  // Создаем класс с именем "Student"
+        {
+            private string name;  //  имя студента
+            private string id;  //  ID студента
+            private int yearofstudy;  //  год обучения студента
+            public Student(string name, string id)  //  Создаем конструктор с двумя параметрами
+            {
+                this.name = name;   //  вводится имя     this пишется чтобы компилятор не перепутал переменные
+                this.id = id;  //  вводится id         
+            }
+            public void Increment()  //  Метод для инкрементирования года обучения обьекта Студента
+            {
+                yearofstudy++;
+            }
+            public int YearofStudy  // Это делается для  доступа в приватному свойству yearofstudy
+            {
+                get
+                {
+                    return yearofstudy;
+                }
+                set
+                {
+                    yearofstudy = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s = new Student("Yerlan", "18BD110739");  //  создаем обьект класса Студент
+            s.YearofStudy = 1;  //  по умолчанию значение равно 1
+            s.Increment();  //  после мметода значение инкрементируется (+1), то есть равно 2
+            Console.WriteLine(s.YearofStudy);
+
+        }
+    }
+}
