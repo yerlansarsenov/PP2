8db31bd216b5ec27e1309ce0ca3c6c115fbc5f0e
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-02-02 13:42:03 +0600

    Add files via upload

---
+++ Week 2/Task2/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool Prime(int a)
+        {
+            if (a <= 1)
+            {
+                return false;
+            }
+            for(int i = 2; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader qw = new StreamReader(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\Text.txt");
+            string arr = qw.ReadToEnd();
+            string[] arra = arr.Split(' ');
+            StreamWriter s = new StreamWriter(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\output.txt");
+           
+            for (int i = 0; i < arra.Length; i++)
+            {
+                int we = int.Parse(arra[i]);
+                
+              
+                if (Prime(we))         
+                {
+                      s.Write(we);
+                    s.Write(" ");               
+                }
+            }
+            s.Close();
+
+           
+         
+            
+        }
+    }
+}
