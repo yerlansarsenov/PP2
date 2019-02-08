cd231fb80e71b26a3d7442bcd971d6bc2b9de120
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-02-08 22:53:59 +0600

    Add files via upload

---
+++ Week 2/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string q = @"C:\Users\user1\Desktop\PP2\Week 2\Task4\Task4\text1.txt";    //  Пути для создания 
+            string w = @"C:\Users\user1\Desktop\PP2\Week 2\Task4\Task4\text2.txt";    //     файлов
+            string asd = "KBTU is the best!!!";
+            StreamWriter dd = new StreamWriter(q);
+            dd.Write(asd);
+            dd.Close();
+            File.Copy(q, w);   //  Копируем  в новый файл
+            File.Delete(q);   //  Удаляем первый файл
+        }
+    }
+}
