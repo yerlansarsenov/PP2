cd5601471cab2066bb444ac7f5213451505d90a8
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-02-14 17:25:02 +0600

    Update Program.cs

--- Week 2/Task3/Task3/Program.cs
+++ Week 2/Task3/Task3/Program.cs
@@ -9,11 +9,11 @@ namespace Task3
 {
     class Program
     {
-        static string Space(int a)
+        static string Space(int a)     // функция для создания иерархии
         {
             string s = "      ";
             string q = "";
-            for(int i = 0; i < a; i++)
+            for(int i = 0; i < a; i++)  // а раз выводим "      "
             {
                 q += s;
             }
@@ -21,26 +21,27 @@ namespace Task3
         }
         static void qweqwe(DirectoryInfo dir, int a)
         {
-            FileInfo[] files = dir.GetFiles();
-            DirectoryInfo[] dires = dir.GetDirectories();
+            FileInfo[] files = dir.GetFiles();    // все файлы папки вводим в массив
+            DirectoryInfo[] dires = dir.GetDirectories();   // также все папки папки вводим в массив
             foreach(FileInfo file in files)
             {
-              
-                Console.Write(Space(a));
+                //   выводим названия всех содержимых файлов
+                Console.Write(Space(a));  // функция для создания некой иерархии
                 Console.WriteLine(file.Name);
             }
             foreach(DirectoryInfo dire in dires)
             {
                 Console.Write(Space(a));
-                Console.WriteLine(dire.Name);
-                qweqwe(dire, a+1);
+                Console.WriteLine(dire.Name);   // выводим названия папки, и сразу вызываем эту же функцию
+                qweqwe(dire, a+1);              //  уже для данной папки, получается своего рода рекурсия
+                    // каждый раз прибавляем значение а, чтобы получилась иерархия
             }
         }
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user1\Desktop\ict");
-            qweqwe(dir, 0);
-            Console.ReadKey();
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user1\Desktop\ict");    // создаем переменную класса DirectoryInfo
+            qweqwe(dir, 0);   //  функция для отображения содержимого папки
+            Console.ReadKey();   // ЧТОБЫ  КОНСОЛЬ НЕ ЗАКРЫЛАСЬ СРАЗУ
         }
     }
 }
