3d00b2c37e63ecdeba98de50014d567cdafb3c6a
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-02-14 17:01:44 +0600

    Update Program.cs

--- Week 2/Task1/ConsoleApp1/Program.cs
+++ Week 2/Task1/ConsoleApp1/Program.cs
@@ -9,29 +9,29 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static bool Polyndrom(string q)
+        public static bool Polyndrom(string q)   // булевая функция для проверки на полиндромность
         {
-            bool qw = true;
-            for(int i = 0; i <= q.Length/2; i++)
+            bool qw = true;   
+            for(int i = 0; i <= q.Length/2; i++)    // пробегаемся до середины текста и проверяем первый и последний элемент на сходство
             {
-                if (q[i] != q[q.Length - 1 - i])
+                if (q[i] != q[q.Length - 1 - i])   //  если не равны то текст является не полиндромом, функция возвращает значение false
                 {
                     qw = false;
                     return qw;
                 }
             }
-            return qw;
+            return qw;    // если же все элементы равны, то функция возвращает значение true
         }
         static void Main(string[] args)
         {
-            string file = File.ReadAllText(@"C:\Users\user1\Desktop\PP2\Week 2\Task1\ConsoleApp1\text.txt");
-            if (Polyndrom(file))
+            string file = File.ReadAllText(@"C:\Users\user1\Desktop\PP2\Week 2\Task1\ConsoleApp1\text.txt");   // стринговая переменная file принимает значение текста в данной path
+            if (Polyndrom(file))    // проверяем текст на полиндромность
             {
-                Console.WriteLine("Yes");
+                Console.WriteLine("Yes");   // если полиндром выводим yes, иначе no
             }
             else Console.WriteLine("No");
 
-            Console.ReadKey();
+            Console.ReadKey();   // ЧТОБЫ КОНСОЛЬ НЕ ЗАКРЫЛАСЬ СРАЗУ
         }
     }
 }
