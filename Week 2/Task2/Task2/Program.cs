d0c82e45a29d014a22afe641f36d2bcf601d1253
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-02-14 17:14:50 +0600

    Update Program.cs

--- Week 2/Task2/Task2/Program.cs
+++ Week 2/Task2/Task2/Program.cs
@@ -9,36 +9,36 @@ namespace Task2
 {
     class Program
     {
-        public static bool Prime(int a)
+        public static bool Prime(int a)   //  булевая функция для проверки протое ли число
         {
-            if (a <= 1)
+            if (a <= 1)    // число 1 или меньше сразу же исключается
             {
                 return false;
             }
-            for(int i = 2; i <= a / 2; i++)
+            for(int i = 2; i <= a / 2; i++)   // если число разделяется без остатка на 2 и больше числа то оно составное
             {
                 if (a % i == 0)
                 {
                     return false;
                 }
             }
-            return true;
+            return true;   // иначе простое
         }
         static void Main(string[] args)
         {
-            StreamReader qw = new StreamReader(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\Text.txt");
-            string arr = qw.ReadToEnd();
-            string[] arra = arr.Split(' ');
-            StreamWriter s = new StreamWriter(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\output.txt");
+            StreamReader qw = new StreamReader(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\Text.txt");  // "инструмент" для чтения текста
+            string arr = qw.ReadToEnd();   //  переменная  arr принимает значение текста Text
+            string[] arra = arr.Split(' ');    // разделяем текст на элементы стрингового массива через пробел
+            StreamWriter s = new StreamWriter(@"C:\Users\user1\Desktop\PP2\Week 2\Task2\Task2\output.txt");  // "инструмент" для ввода текста
            
-            for (int i = 0; i < arra.Length; i++)
+            for (int i = 0; i < arra.Length; i++)   // пробегаемся по количеству элементов массива
             {
-                int we = int.Parse(arra[i]);
+                int we = int.Parse(arra[i]);  // конвертируем каждый элемент массива со стринга в интеджер
                 
               
-                if (Prime(we))         
+                if (Prime(we))         // проверяем простое ли число
                 {
-                      s.Write(we);
+                      s.Write(we);   // если простое, то вводим его в текст output
                     s.Write(" ");               
                 }
             }
