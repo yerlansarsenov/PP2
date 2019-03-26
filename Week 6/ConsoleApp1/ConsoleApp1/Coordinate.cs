fa58a5558773292ec73ffe155f28971b80542a31
yerlansarsenov <47049371+yerlansarsenov@users.noreply.github.com>
2019-03-26 17:48:18 +0600

    Add files via upload

---
+++ Week 6/ConsoleApp1/ConsoleApp1/Coordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Coordinate
+    {
+        public int x, y;
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Week 6/ConsoleApp1/ConsoleApp1/Wall.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Wall : Game
+    {
+       
+        public Wall(char c, ConsoleColor color) : base(0,0,color,c)
+        {
+            cors = new List<Coordinate>();
+        }
+        enum Level
+        {
+            FIRST,
+            SECOND,
+            THIRD
+        }
+        Level level = Level.FIRST;
+        public void LoadLevel()
+        {
+            string lvl = @"C:\Users\user1\Desktop\PP2\Week 6\ConsoleApp1\ConsoleApp1\lvl1.txt";
+            if(level == Level.SECOND)
+            {
+                lvl = @"C:\Users\user1\Desktop\PP2\Week 6\ConsoleApp1\ConsoleApp1\lvl2.txt";
+            }
+            if(level == Level.THIRD)
+            {
+                lvl = @"C:\Users\user1\Desktop\PP2\Week 6\ConsoleApp1\ConsoleApp1\lvl3.txt";
+            }
+            StreamReader st = new StreamReader(lvl);
+            string[] str = st.ReadToEnd().Split('\n');
+            for(int i = 0; i < str.Length; i++)
+            {
+                for(int j = 0; j < str[i].Length; j++){
+                    if (str[i][j] == '#')
+                    {
+                        cors.Add(new Coordinate(j, i));
+                    }
+                }
+            }
+        }
+        public void LevelUp()
+        {
+            if(level == Level.FIRST)
+            {
+                level = Level.SECOND;
+            }
+            if(level == Level.SECOND)
+            {
+                level = Level.THIRD;
+            }
+        }
+    }
+}
